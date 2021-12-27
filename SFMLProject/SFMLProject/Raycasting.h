a280a6267da96aa142d91af9243c395298cbcce0
TayfunBugraBas <73062585+TayfunBugraBas@users.noreply.github.com>
2021-12-27 10:58:50 +0300

    .

--- SFMLProject/SFMLProject/Raycasting.h
+++ SFMLProject/SFMLProject/Raycasting.h
@@ -4,43 +4,15 @@
 #include<iostream>
 #include <math.h>
 #define PI 3.1415926535
+#define P2 PI/2
+#define P3 3*PI/2
+
 
 
 
 
 using namespace sf;
 using namespace std;
-
-class Oyuncu {
-public:
-
-	float px = 300, py = 300;
-	float 	pdx,pdy,pa;
-
-
-	void drawPlayer() {
-
-		glColor3f(1, 1, 0);
-		glPointSize(8);
-		glBegin(GL_POINTS);
-		glVertex2i(px, py);
-		glEnd();
-
-		glLineWidth(3);
-		glBegin(GL_LINES);
-		glVertex2i(px, py);
-		glVertex2i(px + pdx * 5, py + pdy * 5);
-		glEnd();
-
-	}
-
-	void drawRays() {
-
-	}
-
-
-};
-
 class Map {
 
 public:
@@ -51,15 +23,15 @@ public:
 
 	}
 
- int map[64]= {
-		1,1,1,1,1,1,1,1,
-		1,0,0,1,0,0,0,1,
-		1,0,0,1,0,0,0,1,
-		1,0,0,0,0,1,1,1,
-		1,0,0,0,0,0,0,1,
-		1,0,0,1,1,0,0,1,
-		1,0,0,0,0,0,0,1,
-		1,1,1,1,1,1,1,1
+	int map[64] = {/*Sonra Büyüyecek*/
+		   1,1,1,1,1,1,1,1,
+		   1,0,0,0,0,0,0,1,
+		   1,0,1,0,1,0,1,1,
+		   1,0,0,0,0,0,0,1,
+		   1,0,0,1,0,1,0,1,
+		   1,0,0,0,1,0,0,1,
+		   1,0,1,0,0,0,0,1,
+		   1,1,1,1,1,1,1,1
 
 	};
 
@@ -75,17 +47,124 @@ public:
 				}
 				xo = xa * mapS; yo = ya * mapS;
 				glBegin(GL_QUADS);
-				glVertex2i(xo+1, yo+1);
-				glVertex2i(xo+1, yo+mapS-1);
-				glVertex2i(xo+ mapS-1,yo+mapS-1);
-				glVertex2i(xo-1 + mapS,yo+1);
+				glVertex2i(xo + 1, yo + 1);
+				glVertex2i(xo + 1, yo + mapS - 1);
+				glVertex2i(xo + mapS - 1, yo + mapS - 1);
+				glVertex2i(xo - 1 + mapS, yo + 1);
 				glEnd();
 			}
 		}
 	}
 
-	
+
 };
+
+float degToRad(int a) {
+
+	return a * PI / 180.0; /* Bilgisayarlar neden Radyanla çalýþýyor :d */
+
+}
+int AciDuzelt(int a) {
+	if (a > 359) {
+		a -= 360;
+
+	}
+	if (a < 0) {
+		a += 360;
+	}
+	return a;
+}
+
+
+class Oyuncu {
+public:
+	Map avr;
+
+	float px , py ;
+	float 	pdx,pdy,pa;
+
+
+	void drawPlayer() {
+		glColor3f(1, 1, 0);   glPointSize(8);    glLineWidth(4);
+		glBegin(GL_POINTS); glVertex2i(px, py); glEnd();
+		glBegin(GL_LINES);  glVertex2i(px, py); glVertex2i(px + pdx * 20, py + pdy * 20); glEnd();
+
+	}
+
+	float mesafe(float ax, float ay, float bx, float by,  float ang) {
+		return cos(degToRad(ang)) * (bx - ax) - sin(degToRad(ang)) * (by - ay);
+	}
+
+
+
+
+	void drawRays() {
+		glColor3f(0, 1, 1); glBegin(GL_QUADS); glVertex2i(526, 0); glVertex2i(1006, 0); glVertex2i(1006, 160); glVertex2i(526, 160); glEnd();
+		glColor3f(0, 0, 1); glBegin(GL_QUADS); glVertex2i(526, 160); glVertex2i(1006, 160); glVertex2i(1006, 320); glVertex2i(526, 320); glEnd();
+
+		int r, mx, my, mp, dof, side; float vx, vy, rx, ry, ra, xo, yo, disV, disH;
+
+		ra = AciDuzelt(pa + 30);                                                            
+
+		for (r = 0; r < 60; r++)
+		{
+			
+			dof = 0; side = 0; disV = 100000;/*Ellemeyiniz sonra Dünyayý tersten görüyoruz*/
+			float Tan = tan(degToRad(ra));
+			if (cos(degToRad(ra)) > 0.001) { 
+				rx = (((int)px >> 6) << 6) + 64;   
+			ry = (px - rx) * Tan + py; xo = 64; yo = -xo * Tan;
+			
+			}
+			else if (cos(degToRad(ra)) < -0.001) { rx = (((int)px >> 6) << 6) - 0.0001; ry = (px - rx) * Tan + py; xo = -64; yo = -xo * Tan; }
+			else { rx = px; ry = py; dof = 8; }                                                 
+			while (dof < 8)
+			{
+				mx = (int)(rx) >> 6; my = (int)(ry) >> 6; mp = my * avr.mapX + mx;
+				if (mp > 0 && mp < avr.mapX * avr.mapY && avr.map[mp] == 1) { dof = 8; disV = cos(degToRad(ra)) * (rx - px) - sin(degToRad(ra)) * (ry - py); }
+				else { rx += xo; ry += yo; dof += 1; }                                               
+			}
+			vx = rx; vy = ry;
+
+			
+			dof = 0; disH = 100000;/*Ellemeyiniz sonra Dünyayý tersten görüyoruz*/
+			Tan = 1.0 / Tan;
+			if (sin(degToRad(ra)) > 0.001) { ry = (((int)py >> 6) << 6) - 0.0001; rx = (py - ry) * Tan + px; yo = -64; xo = -yo * Tan; }
+			else if (sin(degToRad(ra)) < -0.001) { ry = (((int)py >> 6) << 6) + 64;      rx = (py - ry) * Tan + px; yo = 64; xo = -yo * Tan; }
+			else { rx = px; ry = py; dof = 8; }                                                  
+
+			while (dof < 8)
+			{
+				mx = (int)(rx) >> 6; my = (int)(ry) >> 6; mp = my * avr.mapX + mx;
+				if (mp > 0 && mp < avr.mapX * avr.mapY && avr.map[mp] == 1) { dof = 8; disH = cos(degToRad(ra)) * (rx - px) - sin(degToRad(ra)) * (ry - py); }
+				else { rx += xo; ry += yo; dof += 1; }                                             
+			}
+
+			glColor3f(0, 0.8, 0);
+			if (disV < disH) { rx = vx; ry = vy; disH = disV; glColor3f(0, 0.6, 0); }                  
+			glLineWidth(2); glBegin(GL_LINES); glVertex2i(px, py); glVertex2i(rx, ry); glEnd();
+
+			int ca =AciDuzelt(pa - ra); disH = disH * cos(degToRad(ca));                          
+			int lineH = (avr.mapS * 320) / (disH); if (lineH > 320) { lineH = 320; }                    
+			int lineOff = 160 - (lineH >> 1);                                               
+
+			glLineWidth(8); glBegin(GL_LINES); glVertex2i(r * 8 + 530, lineOff); glVertex2i(r * 8 + 530, lineOff + lineH); glEnd(); 
+
+			ra = AciDuzelt(ra - 1);                                                              
+
+		}
+
+		}
+
+
+
+
+	};
+
+
+
+
+
 
 	Oyuncu o;
 	Map m;
@@ -94,37 +173,19 @@ public:
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		m.loadLevelMap();
+		o.drawRays();
 		o.drawPlayer();
 		glutSwapBuffers();
+		
 
 	}
 
 	void tuslar(unsigned char key, int x, int y) {
 
-		if (key == 'A' || key == 'a') {
-			o.pa = o.pa - 0.1;
-			if (o.pa < 0) {
-				o.pa = o.pa + 2 * PI;
-			}
-			o.pdx = cos(o.pa) * 5;
-			o.pdy = sin(o.pa) * 5;
-		}
-		if (key == 'D' || key == 'd') {
-			o.pa = o.pa + 0.1;
-			if (o.pa > 2*PI) {
-				o.pa = o.pa - 2 * PI;
-			}
-			o.pdx = cos(o.pa) * 5;
-			o.pdy = sin(o.pa) * 5;
-		}
-		if (key == 'W' || key == 'w') {
-			o.px = o.px + o.pdx;
-			o.py = o.py + o.pdy;
-		}
-		if (key == 'S' || key == 's') {
-			o.px = o.px - o.pdx;
-			o.py = o.py - o.pdy;
-		}
+		if (key == 'a') { o.pa += 5; o.pa = AciDuzelt(o.pa); o.pdx = cos(degToRad(o.pa)); o.pdy = -sin(degToRad(o.pa)); }
+		if (key == 'd') { o.pa -= 5; o.pa = AciDuzelt(o.pa); o.pdx = cos(degToRad(o.pa)); o.pdy = -sin(degToRad(o.pa)); }
+		if (key == 'w') { o.px += o.pdx * 5; o.py += o.pdy * 5; }
+		if (key == 's') { o.px -= o.pdx * 5; o.py -= o.pdy * 5; }
 		glutPostRedisplay();
 
 	}
@@ -144,11 +205,12 @@ public:
 
 		glClearColor(0.3, 0.3, 0.3, 0);
 		gluOrtho2D(0, 1024, 512, 0);
-		o.px = 300;
-		o.py = 300;
-		o.pdx = cos(o.pa) * 5;
-		o.pdy = sin(o.pa) * 5;
+		o.px = 150;
+		o.py = 400;
+		o.pa = 90;
+		o.pdx = cos(degToRad(o.pa)); o.pdy = -sin(degToRad(o.pa));
 	}
+	
 
 	void RayMain(int argc, char* argv[]) {
 		
